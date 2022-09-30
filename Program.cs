92c599d6d473826b7aa291d41daeecb893ef1be6
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:52:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Solución_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma = 0;
+
+            Console.WriteLine("Ingrese el primer numero ");
+
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero ");
+
+            num2 = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = num1; i <= num2; i++)
+
+            {
+
+                suma = suma + i;
+
+
+
+            }
+
+            Console.WriteLine("La suma de los 2 numeros es: " + suma);
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
