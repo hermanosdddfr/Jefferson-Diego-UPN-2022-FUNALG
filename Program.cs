876f29c95501893da38645b7f42852294abec698
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:55:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Solución_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char caracter;
+            caracter = 'a';
+            caracter = 'e';
+            caracter = 'i';
+            caracter = 'o';
+            caracter = 'u';
+            do
+            {
+                Console.Write("Ingrese un numero seguido de ENTER: ");
+                caracter = char.Parse(Console.ReadLine());
+            }
+            while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u');
+
+
+        }
+    }
+}
