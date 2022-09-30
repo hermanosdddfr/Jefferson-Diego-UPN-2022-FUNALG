e8ee00a9a6036ba25fca21326648759547f0ed24
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:47:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Solución_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            for (int num = 2; num <= 100; num++)
+            {
+                for (int num2 = 1; num2 <= num; num2++)
+                {
+                    if(num%num2== 0)
+                    {
+                        cont=cont+1;
+                    }
+
+                }
+                if (cont <= 2)
+                {
+                    Console.WriteLine(num);
+                }
+                cont = 0;
+            }
+            
+               
+
+
+
+
+
+            
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
