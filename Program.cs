c90ced5a00ca96270fdfa08511be20279d14aa16
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:56:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solución_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caracter;
+            do
+            {
+                Console.WriteLine("¿Desea continuar? S/N: ");
+                caracter = Console.ReadLine();
+            }
+            while (caracter != "S");
+            
+
+        }
+    }
+}
