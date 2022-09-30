69b0b6b544a9ea0bf3371aaa69d01f9fccd18e10
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:54:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Solución_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            do
+
+            {
+
+                Console.Write("INGRESE UN NUMERO SEGUID0 DE ENTER: ");
+
+                numero = int.Parse(Console.ReadLine());
+
+
+
+            }
+
+            while (numero <= 100);
+
+            Console.Write("Error!!! Numero fuera de rango: ");
+
+
+
+
+
+        }
+    }
+}
