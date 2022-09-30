8a3d64b5e3cd3ef66b3acc680fcc7fd7360d2ede
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:35:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Solución_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Ingrese un numero");
+                num=int.Parse(Console.ReadLine());
+            } while (num < 1 || num > 5);
+        } 
+    }
+}
