29a74ec22a8d719ae53f22e0fe8dc43cdd826c88
hermanosdddfr <114704230+hermanosdddfr@users.noreply.github.com>
2022-09-30 07:52:58 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 
-namespace Solución_4
+namespace Solución_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int num1, num2, suma = 0;
+            int num, suma = 0;
 
-            Console.WriteLine("Ingrese el primer numero ");
+            Console.WriteLine("ESCRIBA UN NUMERO: ");
 
-            num1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingrese el segundo numero ");
-
-            num2 = int.Parse(Console.ReadLine());
+            num = int.Parse(Console.ReadLine());
 
 
 
-            for (int i = num1; i <= num2; i++)
+            for (int i = 1; i <= num; i = i + 2)
 
             {
 
@@ -28,20 +24,7 @@ namespace Solución_4
 
             }
 
-            Console.WriteLine("La suma de los 2 numeros es: " + suma);
-
-
-
-           
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("La suma de numeros impares es : " + suma);
 
 
 
